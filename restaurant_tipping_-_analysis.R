85fc04a13c67c5b3802bc5c4e2df110a7fce6ed2
Christian Bitter <christian.bitter@gmail.com>
2019-02-16 21:25:26 +0100

    added some plot and a test for the autocorrelation of residuals, to see if the assumptions around the error terms are violated.

--- restaurant_tipping_-_analysis.R
+++ restaurant_tipping_-_analysis.R
@@ -3,6 +3,7 @@ rm(list = ls());
 library(ggplot2);
 library(ggthemes);
 library(dplyr);
+library(lmtest);
 
 df <- read.csv("restaurant_tipping.csv", header = T, sep = ";", dec = ",");
 
@@ -27,6 +28,8 @@ df %>%
 
 df$tip_per_unit <- df$Yt / df$Xt;
 
+df %>% ggplot(aes(y = Yt, x = as.factor(1))) + geom_boxplot() + theme_light() + 
+  labs(y = "Tip Amount [€]", title = "Range/ Spread of Tip Amounts", x = "");
 df %>% ggplot(aes(y = tip_per_unit)) + geom_boxplot() + theme_light();
 
 df %>% 
@@ -51,10 +54,24 @@ summary(lm.2);
 lm.21 <- lm(Yt ~ Xt - 1, df);
 summary(lm.21);
 
-# test the assumptions of the model - correlation of residuals
+cat("test the assumptions of the model - correlation of residuals\r\n");
 # plot the residuals and residuals vs. attributes
 # perform a test for autocorrelated residuals
 r <- residuals(lm.21);
+# in order to check for association we regress residuals - the simplest approach
+# and now can check for the slope coefficient's signifance
+# we could also aim for the breusch godfrey test or the durbin-watson test
+# https://en.wikipedia.org/wiki/Breusch%E2%80%93Godfrey_test
+n <- length(r) 
+lm_r_check <- lm(r[-n] ~ r[-1]) 
+summary(lm_r_check);
+# we see that the estimate for the slope is not significant, meaning, that 
+# the data does not support the assumption of a non-zero slope
+# meaning that there is no association between the residuals
+# meaning that they are not correlated.
+cat("Residuals are correlated (Breusch Godfrey Test) - p = 0.05: ", (lmtest::bgtest(lm.21)$p.value < .05), "\r\n");
+cat("Residuals are correlated (Durbin Watson Test) - p = 0.05: ", (lmtest::dwtest(lm.21)$p.value < .05), "\r\n");
+
 yhat <- predict(lm.21);
 
 df$residuals <- r;
@@ -68,7 +85,7 @@ df %>% ggplot(aes(x = yhat, y = residuals)) + geom_point() +
   labs(x = "Predicted Tip Amount [€]",
        y = "Residuals", title = "Residual vs. Predictor plot");
 
-# test the assumptions of the model - heteroscedasticity
+# test the assumptions of the model - homogenity of variance
 # plot and perform a test of variance
 df %>% ggplot(aes(x = yhat, y = residuals)) + geom_point() + 
   labs(x = "Predicted Tip Amount [€]",
